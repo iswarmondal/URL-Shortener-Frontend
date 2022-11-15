ebdce005257d4c5cddfddee0e8853a11565d5a23
Iswar <rick731202@gmail.com>
2022-11-15 10:31:24 +0530

    Some bug fixed

--- src/authAxios.js
+++ src/authAxios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const authAxios = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
     headers:{
-        Authorization: `Bearer ${localStorage.getItem("authToken")}`
+        Authorization: `${localStorage.getItem("authToken")}`
     }
 })
 

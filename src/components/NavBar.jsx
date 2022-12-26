9b1b94ff0e264edac3017400b5af85b9c0105469
Iswar <rick731202@gmail.com>
2022-12-26 11:10:05 +0530

    manifest.json changed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import { MenuItem } from '@mui/material';
 
 function NavBar() {
